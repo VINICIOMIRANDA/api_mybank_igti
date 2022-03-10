20b64391d52a783f819e56bba23b686662b9a53a
vinicio.covalski <vinicio.covalski@gmail.com>
2022-03-09 21:27:06 -0300

    Atribuido o repositories no projeto

--- routes/account.routes.js
+++ routes/account.routes.js
@@ -3,12 +3,12 @@ import AccountController from "../controllers/account.controller.js";
 
 const router = express.Router();
 
-router.post("/", AccountController.createAccount );
+router.post("/", AccountController.createAccount);
 router.get("/", AccountController.getAccounts);
 router.get("/:id", AccountController.getAccountsId);
 router.delete("/:id", AccountController.deleteAccount);
 router.put("/",AccountController.updateAccount);
-router.patch("/updateBalance", AccountController.updateBalance ); 
+router.patch("/updateBalance", AccountController.updateBalance); 
 
 router.use((err, req, res, next) => {
   global.loggers.error(`${req.method} ${req.baseUrl} - ${err.message}`);
--- services/account.service.js
+++ services/account.service.js
@@ -1,70 +1,29 @@
-import {  promises as fs } from "fs";
-const { readFile, writeFile } = fs;
-
+import AccountRepository from "../repositories/account.repository.js";
 
 async function createAccount(account) {
-  const data = JSON.parse(await readFile(global.filename));
-
-  account = {
-    id: data.nextId++,
-    name: account.name,
-    balance: account.balance,
-  };
-
-  data.accounts.push(account);
-
-  await writeFile(global.filename, JSON.stringify(data, null, 2));
-
-  return account;
+    return await AccountRepository.insertAccount(account);
 }
 
 async function getAccounts(){
-    const data = JSON.parse(await readFile(global.filename));
-    delete data.nextId;
-    return data;
+    return await AccountRepository.getAccounts();
 }
 
 async function getAccountsId(id){
-    const data = JSON.parse(await readFile(global.filename));
-    const account = data.accounts.find(
-      (account) => account.id === parseInt(id));
-        return account;
+  return await AccountRepository.getAccountsId(id);
 }
 
 async function deleteAccount(id){
-    const data = JSON.parse(await readFile(global.filename));
-    data.accounts = data.accounts.filter(
-      (account) => account.id !== parseInt(id));
-    await writeFile(global.filename, JSON.stringify(data, null, 2));
+    return await AccountRepository.deleteAccount(id);
 }
 
 async function updateAccount(account){
-    const data = JSON.parse(await readFile(global.filename));
-    const index = data.accounts.findIndex((a) => a.id === account.id);
-
-    if (index === -1) {
-      throw new Error("Registro não encontrado");
-    }
-    data.accounts[index].name = account.name;
-    data.accounts[index].balance = account.balance;
-    await writeFile(global.filename, JSON.stringify(data, null, 2));
-
-    return data.accounts[index];
+  return await AccountRepository.updateAccount(account);
 }
 
 async function updateBalance(account){
-
-    const data = JSON.parse(await readFile(global.filename));
-    const index = data.accounts.findIndex((a) => a.id === account.id);
- 
-    if (index === -1) {
-      throw new Error("Registro não encontrado");
-    }
-
-    data.accounts[index].balance = account.balance;
-    await writeFile(global.filename, JSON.stringify(data, null, 2));
-
-    return data.accounts[index];
+  const acc = await AccountRepository.getAccountsId(account.id);
+  acc.balance = account.balance;
+  return await AccountRepository.updateAccount(acc);
 }
 
 export default {
